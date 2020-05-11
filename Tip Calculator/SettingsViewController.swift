33bf4de8532ae58cea54e20abf577f201faf15b7
Priya Pahal <ppahal@ucsd.edu>
2020-05-10 18:19:59 -0700

    Tech Fellow App

---
+++ Tip Calculator/SettingsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SettingsViewController.swift
+//  Tip Calculator
+//
+//  Created by Priya Pahal on 5/10/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipField1: UITextField!
+    @IBOutlet weak var tipField2: UITextField!
+    @IBOutlet weak var tipField3: UITextField!
+    
+    var tip1 = 0.00
+    var tip2 = 0.00
+    var tip3 = 0.00
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let tipCalculatorVC = segue.destination as! ViewController
+        tipCalculatorVC.tip1 = tip1
+        tipCalculatorVC.tip2 = tip2
+        tipCalculatorVC.tip3 = tip3
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tipField1.text = "\(integer_t(tip1*100))"
+        tipField2.text = "\(integer_t(tip2*100))"
+        tipField3.text = "\(integer_t(tip3*100))"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func enterNewTips(_ sender: Any) {
+        tip1 = Double(tipField1.text!) ?? 0
+        tip1 = tip1/100
+        tip2 = Double(tipField2.text!) ?? 0
+        tip2 = tip2/100
+        tip3 = Double(tipField3.text!) ?? 0
+        tip3 = tip3/100
+        
+    }
+}
--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Tip Calculator
 //
-//  Created by Marco Antonio Serpas on 1/22/20.
+//  Created by Priya Pahal on 1/22/20.
 //  Copyright © 2020 Codepath. All rights reserved.
 //
 
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    var tip1 = 0.15
+    var tip2 = 0.18
+    var tip3 = 0.20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let settingsVC = segue.destination as! SettingsViewController
+        settingsVC.tip1 = tip1
+        settingsVC.tip2 = tip2
+        settingsVC.tip3 = tip3
+    }
+    
+    @IBAction func unwindToTC(_ sender: UIStoryboardSegue){}
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+        
+    }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get
+        let bill = Double(billField.text!) ?? 0
+        
+        // Calculate
+        let tipPercentages = [tip1, tip2, tip3]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update
+        tipControl.setTitle("\(integer_t(tip1*100))%", forSegmentAt: 0)
+        tipControl.setTitle("\(integer_t(tip2*100))%", forSegmentAt: 1)
+        tipControl.setTitle("\(integer_t(tip3*100))%", forSegmentAt: 2)
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
 }
 
