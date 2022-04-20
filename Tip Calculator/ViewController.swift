6cadcea4b0e7427648570110f115b7ae08da61ac
ppahal <ppahal@ucsd.edu>
2022-04-20 09:01:31 -0700

    changes to AI & cleaning up code

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,62 +9,91 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Outlets
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    // Variables
     var tip1 = 0.15
     var tip2 = 0.18
     var tip3 = 0.20
     var locale = NSLocale.current
     
+    /*
+     * When the view loads do the following...
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Setup the keyboard for inputting our total
         billField.keyboardType = UIKeyboardType.decimalPad
         billField.keyboardAppearance = UIKeyboardAppearance.dark
-        // Do any additional setup after loading the view.
-        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: { () -> Void in //self.trayView.center = self.trayDown }, completion: nil)
-        }, completion: nil)
+        
+        // Possible Animation?
+//        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: { () -> Void in //self.trayView.center = self.trayDown }, completion: nil)
+//        }, completion: nil)
+        
+        // UserDefaults
         let userDefaults = UserDefaults.standard
-        tip1 = userDefaults.double(forKey: "tip1")
-        tip2 = userDefaults.double(forKey: "tip2")
-        tip3 = userDefaults.double(forKey: "tip3")
+        if userDefaults.double(forKey: "tip1") != 0.00{
+            tip1 = userDefaults.double(forKey: "tip1")
+        }
+        if userDefaults.double(forKey: "tip2") != 0.00{
+            tip2 = userDefaults.double(forKey: "tip2")
+        }
+        if userDefaults.double(forKey: "tip3") != 0.00{
+            tip3 = userDefaults.double(forKey: "tip3")
+        }
+        
+        // Call the tip calculation function
         calculateTip((Any).self)
-       // NotificationCenter.default.addObserver(self, selector: #selector(billField.textDidChangeNotification), name: NSNotification.Name.textDidChangeNotification, object: nil)
     }
     
+    /*
+     * Prepares for the switch to the Settings Screen.
+     */
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let settingsVC = segue.destination as! SettingsViewController
-        //Initialize Tips
+        // Set the tips
         settingsVC.tip1 = tip1
         settingsVC.tip2 = tip2
         settingsVC.tip3 = tip3
     }
     
+    /*
+     * When the view appears do the following...
+     */
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        // Set the keyboard to appear
         self.billField.becomeFirstResponder()
+        // Set the currency to the phone's current country
         locale = NSLocale.autoupdatingCurrent
-        let userDefaults = UserDefaults.standard
-        if(userDefaults.object(forKey: "Last Date") != nil){
-            let previousDate = userDefaults.object(forKey: "Last Date") as! Date
-            let billValue = userDefaults.object(forKey: "Bill") as! String
-            let currentDate = Date()
-            let time = currentDate.timeIntervalSince(previousDate)
-            if(time <= 600){
-                billField.text = billValue
-            }
-        }
+        // FIXME: More user default stuff
+//        let userDefaults = UserDefaults.standard
+//        if(userDefaults.object(forKey: "Last Date") != nil){
+//            let previousDate = userDefaults.object(forKey: "Last Date") as! Date
+//            let billValue = userDefaults.object(forKey: "Bill") as! String
+//            let currentDate = Date()
+//            let time = currentDate.timeIntervalSince(previousDate)
+//            if(time <= 600){
+//                billField.text = billValue
+//            }
+//        }
     }
     
+    /*
+     * When the view disappears do the following...
+     */
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        let userDefaults = UserDefaults.standard
-        //userDefaults.set(billField.text, forKey: "Bill")
-        userDefaults.set(Date(), forKey: "Last Date")
+//        let userDefaults = UserDefaults.standard
+//        //userDefaults.set(billField.text, forKey: "Bill")
+//        userDefaults.set(Date(), forKey: "Last Date")
     }
+    
     
     @IBAction func unwindToTC(_ sender: UIStoryboardSegue){
         let userDefaults = UserDefaults.standard
